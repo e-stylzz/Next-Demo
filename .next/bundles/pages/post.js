84e5be9052d8ccab71901cd95d93d608490c9e9e
Eric Barb <eric.barb@gmail.com>
2017-05-22 00:25:23 -0400

    More code

---
+++ .next/bundles/pages/post.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/post', function() {
+            var comp = module.exports=webpackJsonp([5],{104:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=l(1),r=u(n),o=l(65),a=u(o);t.default=function(e){return r.default.createElement(a.default,null,r.default.createElement("h1",null,e.url.query.title),r.default.createElement("p",null,"This is the blog post content"))}},217:function(e,t,l){e.exports=l(104)}},[217]);
+            return { page: comp.default }
+          })
+        
--- .next/dist/pages/index.js
+++ .next/dist/pages/index.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _style = require('styled-jsx/style.js');
+
+var _style2 = _interopRequireDefault(_style);
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -12,20 +16,37 @@ var _MyLayout = require('../components/MyLayout.js');
 
 var _MyLayout2 = _interopRequireDefault(_MyLayout);
 
+var _link = require('next/dist/lib/link.js');
+
+var _link2 = _interopRequireDefault(_link);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _jsxFileName = '/Users/ericbarb/Webapps/NextJS/hello-next/pages/index.js?entry';
+function getPosts() {
+    return [{ id: 'hello-nextjs', title: 'Hello Next.js' }, { id: 'learn-nextjs', title: 'Learn Next.js is awesome' }, { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }];
+}
+
+var PostLink = function PostLink(_ref) {
+    var post = _ref.post;
+    return _react2.default.createElement('li', {
+        'data-jsx': 564931920
+    }, _react2.default.createElement(_link2.default, { as: '/p/' + post.id, href: '/post?title=' + post.title }, _react2.default.createElement('a', {
+        'data-jsx': 564931920
+    }, post.title)), _react2.default.createElement(_style2.default, {
+        styleId: 564931920,
+        css: 'li[data-jsx="564931920"] {list-style: none;margin: 5px 0;}a[data-jsx="564931920"] {text-decoration: none;color: blue;}a[data-jsx="564931920"]:hover {opacity: 0.6;}'
+    }));
+};
 
 exports.default = function () {
-    return _react2.default.createElement(_MyLayout2.default, {
-        __source: {
-            fileName: _jsxFileName,
-            lineNumber: 4
-        }
-    }, _react2.default.createElement('p', {
-        __source: {
-            fileName: _jsxFileName,
-            lineNumber: 5
-        }
-    }, 'Hello Next.js'));
+    return _react2.default.createElement(_MyLayout2.default, null, _react2.default.createElement('h1', {
+        'data-jsx': 428171708
+    }, 'My Blog'), _react2.default.createElement('ul', {
+        'data-jsx': 428171708
+    }, getPosts().map(function (post) {
+        return _react2.default.createElement(PostLink, { key: post.id, post: post });
+    })), _react2.default.createElement(_style2.default, {
+        styleId: 428171708,
+        css: 'h1[data-jsx="428171708"],a[data-jsx="428171708"] {font-family: "Arial";}ul[data-jsx="428171708"] {padding: 0;}'
+    }));
 };
